280500472193a38463d5098986b64773e166fafb
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-04-22 13:28:49 +0530

    Add: added loading animation to sign up page using react-spinner library

--- src/screens/signUp/SignUp.js
+++ src/screens/signUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './signUp.css'
 import { NavLink, useNavigate } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
@@ -7,15 +7,28 @@ import {useDispatch} from 'react-redux'
 import { setSignedInUserId } from '../../redux/slices/userSlice';
 import {db} from '../../firebase/app'
 import toast, { Toaster } from 'react-hot-toast';
+import BeatLoader from "react-spinners/BeatLoader";
 
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+  position: "absolute",
+  top: "10%",
+  // backgroundColor: "black"
+};
 
 //TODO
 // make functionality for users to add profile  name and other customizations
 const SignUp = () => {
 
+  const [loading, setLoading] = useState(false);
+  const [color, setColor] = useState("#36d7b7");
+
   // const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
+
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
@@ -39,6 +52,7 @@ const SignUp = () => {
     }
 
     const auth = getAuth();
+    setLoading(true);
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -46,11 +60,13 @@ const SignUp = () => {
         const user = userCredential.user;
         dispatch(setSignedInUserId(user.uid));
         createNewUserInDataBase(user.uid);
+        setLoading(false);
         toast.success("SignUp Complete");
         navigate('/signIn')
         // ...
       })
       .catch((error) => {
+        setLoading(false);
         const errorCode = error.code;
         const errorMessage = error.message;
         console.error(errorMessage);
@@ -63,13 +79,22 @@ const SignUp = () => {
   return (
     <div className='signup_container'>
       <Toaster/>
-      <form onClick={signUpHandler}>
+      <form >
         {/* <input ref={nameRef} type='text' placeholder='Enter user name'/> */}
         <input ref={emailRef} type='email' placeholder='Enter email'/>
         <input ref={passwordRef} type='password' placeholder='Enter password'/>
-        <button >SignUp</button>
+        <button onClick={signUpHandler} >SignUp</button>
       </form>
       <NavLink to={'/signIn'} >SignIn</NavLink>
+      <BeatLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={25}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        speedMultiplier = "1"
+      />
     </div>
   )
 }
